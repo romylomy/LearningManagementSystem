6db661b91471f2bf6ef52f45bc419280807a0f57
Jerome Corpuz <jerome.corpuz@edu.sait.ca>
2023-10-16 15:16:32 -0600

    added Prisma ORM and toast

--- app/(dashboard)/(routes)/Teacher/Courses/page.tsx
+++ app/(dashboard)/(routes)/Teacher/Courses/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from  'next/link'
 import { Button } from '@/components/ui/button'
 
-export default function page() {
+export default function CoursePage() {
   return (
     <div className='p-6'>
         <Link href="/Teacher/Create">
